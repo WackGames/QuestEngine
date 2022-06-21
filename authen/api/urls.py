c8dd92e1df9053952a3072496b50cbfdc06f2825
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-06-21 15:03:16 -0600

    fixed the files to match dependency updates

--- authen/api/urls.py
+++ authen/api/urls.py
@@ -3,7 +3,7 @@ from authen.api.views import (ApiAuthView)
 
 app_name = 'QE'
 urlpatterns = [
-        path(f'', include('djoser.urls'), name='Auth Working'),
-        path(f'', include('djoser.urls.authtoken'), name='Auth Working'),
+        path('', include('djoser.urls'), name='Auth Working'),
+        path('', include('djoser.urls.authtoken'), name='Auth Working'),
 
 ]
--- questengine/settings.py
+++ questengine/settings.py
@@ -16,7 +16,7 @@ from corsheaders.defaults import default_headers
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = str(Path(__file__).resolve().parent.parent)
 
 
 # Quick-start development settings - unsuitable for production
